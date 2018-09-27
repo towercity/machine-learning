ca2174f90eb1e7b13dc096aa4b40870f8cd027fd
towercity <matthewjnerger3@gmail.com>
2018-09-27 17:36:27 -0400

    Add printout functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 function teachDexter() {
   var textBox = document.getElementById("knowledge");
   var response = document.getElementById("response");
+  var userString = document.getElementById("text").value;
 
   textBox.style.display = "none";
-  response.style.display = "inline";
+  response.innerHTML = "Wow...<br>" +
+                        "I didn't know that " +
+                        '"' + userString + '"<br>' +
+                        "Thank you...<br> I am learning...";
 }
