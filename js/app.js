cd8c9cca6b75c2567e04258a9a6fc35066636e83
towercity <matthewjnerger3@gmail.com>
2018-09-30 09:21:52 -0400

    Clear text from textbox in function

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 function teachDexter() {
-  var textBox = document.getElementById("knowledge");
+  var textBox = document.getElementById("text");
   var response = document.getElementById("response");
-  var userString = document.getElementById("text").value;
+  var userString = textBox.value;
+
+  //clear textbox, delayed so user is disctracted by modal
+  setTimeout(function() { textBox.value = ''; }, 200);
 
   //textBox.style.display = "none";
   response.innerHTML = "Wow...<br>" +
                         "I didn't know that " +
                         '"' + userString + '"<br>' +
                         "Thank you...<br> I am learning...";
-
-  console.log("func runs");
 }
