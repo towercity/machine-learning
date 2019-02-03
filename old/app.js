4f9ebd8dcdb76f46df6403a2f1f5d19ecca15d7e
towercity <matthewjnerger3@gmail.com>
2019-02-03 14:49:09 -0500

    Multiline text functionality returns after I accidentally deleted it

--- old/app.js
+++ old/app.js
@@ -2,19 +2,28 @@ var canvas = document.getElementById("machineScreen");
 var context = canvas.getContext("2d");
 
 var Game = {
-  render: function(frame, textFrame) {
-    this.draw(frame, textFrame);
+  render: function(frame) {
+    //console.log("TextFrame: " + this.textFrame);
+    //console.log("TextLine: " + this.textLine);
+
+    this.speed = 80;
+    this.draw(frame);
 
     this.displayText = this.textsArray[this.displayIndex];
     var newFrame = frame + 1;
-    var newTextFrame = textFrame + 1;
 
-    if (Game.displayText != Game.screenText) {
-      Game.screenText += Game.displayText[textFrame];
+    if (this.displayText[this.textLine] != this.screenText[this.textLine]) {
+      this.screenText[this.textLine] += this.displayText[this.textLine][this.textFrame];
     } else {
-      newTextFrame = 0;
-      if (this.keyPressed) {
-        this.nextText();
+      if(this.textLine < this.displayText.length - 1) {
+        this.textLine++;
+        this.textFrame = -1;
+        this.speed = 400;
+      } else {
+        this.textFrame = -1;
+        if (this.keyPressed) {
+          this.nextText();
+        }
       }
     }
 
@@ -24,51 +33,74 @@ var Game = {
     // to fix it
     this.keyPressed = false;
 
+    this.textFrame++;
+
     setTimeout(function() {
-      Game.render(newFrame, newTextFrame);
-    }, 100);
+      Game.render(newFrame);
+    }, this.speed);
   },
 
-  draw: function(frame, textFrame) {
+  draw: function(frame) {
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     // Draw the text box
     context.strokeStyle = "#29ADFF";
-    context.fillStyle = "rgba(0,0,0,0.7)";
+    context.fillStyle = "rgba(0,0,0,1)";
     context.fillRect(20, (canvas.height - 170), (canvas.width - 40), 150);
     context.strokeRect(20, (canvas.height - 170), (canvas.width - 40), 150);
 
     // Draw the text
     context.fillStyle = "#29ADFF";
     context.font = "20px 'courier'";
-    context.fillText((">> " + this.screenText), 40, (canvas.height - 140));
+    context.fillText((this.screenText[0]), 40, (canvas.height - 140));
+    context.fillText((this.screenText[1]), 40, (canvas.height - 110));
+    context.fillText((this.screenText[2]), 40, (canvas.height - 80));
+    context.fillText((this.screenText[3]), 40, (canvas.height - 50));
 
     // Blinking Cursor
-    if( textFrame != 0 ) {
-      context.fillRect((context.measureText(this.screenText).width + 80), (canvas.height - 156), 4, 20);
+    if( this.textFrame != 0 ) {
+      context.fillRect((context.measureText(this.screenText[this.textLine]).width + 45), ((canvas.height - 156) + (this.textLine * 30)), 4, 20);
     } else {
       if ((frame % 6 == 0) || ((frame + 1) % 6 == 0)) {
-        context.fillRect((context.measureText(this.screenText).width + 80), (canvas.height - 156), 4, 20);
+        context.fillRect((context.measureText(this.screenText[this.textLine]).width + 45), ((canvas.height - 156) + (this.textLine * 30)), 4, 20);
       }
     }
   },
 
   nextText: function() {
     if(this.displayIndex < (this.textsArray.length - 1)) {
-      this.screenText = "";
+      this.screenText = ["","","",""];
       this.displayIndex = this.displayIndex + 1;
+      this.textLine = 0;
+      this.textFrame = -1;
     }
   },
 
-  displayText: "",
-  screenText: "",
+  displayText: ["","","",""],
+  screenText: ["","","",""],
 
   textsArray: [
-    "Hello! My name is Dexter Learn......",
-    "Tell me something new......"
+    [
+      ">> Hello! My name is Dexter Learn.",
+      "   ..."
+    ],
+    [
+      ">> Tell me something new......",
+      "   ..."
+    ],
+    [
+      ">> Tell me something new......",
+      "   ...",
+      "   ...third line",
+      "   ... me somet me somet me somet me somet me somet me somet"
+    ]
   ],
   displayIndex: 0,
-  keyPressed: false
+  textLine: 0,
+  textFrame: 0,
+
+  keyPressed: false,
+  speed: 80
 }
 
 function isEnterPressed (e) {
