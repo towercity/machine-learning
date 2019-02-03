8448303fb97810a1e0b78c9ba8709d696172cf0e
towercity <matthewjnerger3@gmail.com>
2019-02-02 20:23:15 -0500

    Move canvas into game logic

--- old/app.js
+++ old/app.js
@@ -1,9 +1,20 @@
 var canvas = document.getElementById("machineScreen");
 var context = canvas.getContext("2d");
 
-context.strokeStyle = "#29ADFF";
-context.strokeRect(20, (canvas.height - 170), (canvas.width - 40), 150);
+var Game = {
+  render: function() {
+    // Draw the text box
+    context.strokeStyle = "#29ADFF";
+    context.fillStyle = "rgba(0,0,0,0.8)";
+    context.fillRect(20, (canvas.height - 170), (canvas.width - 40), 150);
+    context.strokeRect(20, (canvas.height - 170), (canvas.width - 40), 150);
 
-context.fillStyle = "#29ADFF";
-context.font = "20px 'VT323', 'courier'";
-context.fillText(">> Hello! My name is Dexter Learn! ...", 40, (canvas.height - 140));
+    // Draw the text
+    context.fillStyle = "#29ADFF";
+    context.font = "20px 'VT323', 'courier'";
+    context.fillText(this.displayText, 40, (canvas.height - 140));
+  },
+  displayText: ">> Hello! My name is Dexter Learn! ..."
+}
+
+Game.render();
