4fff437f7bf3a8ff0947c4dd266e6de0f2755a7b
towercity <matthewjnerger3@gmail.com>
2019-02-02 20:16:40 -0500

    Update text

--- old/app.js
+++ old/app.js
@@ -1,2 +1,9 @@
 var canvas = document.getElementById("machineScreen");
 var context = canvas.getContext("2d");
+
+context.strokeStyle = "#29ADFF";
+context.strokeRect(20, (canvas.height - 170), (canvas.width - 40), 150);
+
+context.fillStyle = "#29ADFF";
+context.font = "20px 'VT323', 'courier'";
+context.fillText(">> Hello! My name is Dexter Learn! ...", 40, (canvas.height - 140));
