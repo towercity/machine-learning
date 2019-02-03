fe40c75aa347fb9043ecb77931a7cb2b02397faf
towercity <matthewjnerger3@gmail.com>
2019-02-02 22:03:37 -0500

    Mkae blinker blink when not moving

--- old/app.js
+++ old/app.js
@@ -2,8 +2,10 @@ var canvas = document.getElementById("machineScreen");
 var context = canvas.getContext("2d");
 
 var Game = {
-  render: function(textFrame) {
+  render: function(frame, textFrame) {
     context.clearRect(0, 0, canvas.width, canvas.height);
+
+    var newFrame = frame + 1;
 
     // Draw the text box
     context.strokeStyle = "#29ADFF";
@@ -16,24 +18,28 @@ var Game = {
     context.font = "20px 'courier'";
     context.fillText(this.screenText, 40, (canvas.height - 140));
 
-    // Blinker
+    // Blinking Cursor
     if( textFrame != 0 ) {
       context.fillRect((context.measureText(this.screenText).width + 44), (canvas.height - 156), 4, 20);
+    } else {
+      if ((frame % 6 == 0) || ((frame + 1) % 6 == 0)) {
+        context.fillRect((context.measureText(this.screenText).width + 44), (canvas.height - 156), 4, 20);
+      }
     }
 
     setTimeout(function() {
       if (Game.displayText != Game.screenText) {
         Game.screenText += Game.displayText[textFrame];
 
-        Game.render(textFrame + 1);
+        Game.render(newFrame, textFrame + 1);
       } else {
-        Game.render(0);
+        Game.render(newFrame, 0);
       }
     }, 100, this);
   },
 
-  displayText: ">> Hello! My name is Dexter Learn! ...",
+  displayText: ">> Hello! My name is Dexter Learn...",
   screenText: "",
 }
 
-Game.render(0);
+Game.render(0, 0);
